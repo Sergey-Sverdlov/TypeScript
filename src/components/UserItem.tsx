29c1ab85d5c6cee55bd5b461aba1aa6db59f351e
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-01 04:02:05 +0300

    add page with userItem

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {IUser} from "../types/types";
-
+import {useNavigate} from "react-router-dom";
 interface UserItemProps {
-    user: IUser
+    user: IUser;
 }
 const UserItem: FC<UserItemProps> = ({user}) => {
+    const navigate = useNavigate()
     return (
-        <div style={{padding: 15, border: '1px solid black'}}>
+        <div onClick={()=>navigate(`/users/${user.id}`)} style={{padding: 15, border: '1px solid black'}}>
             {user.id}. {user.name} Проживает в городе &nbsp;
             {user.address.city} на улице {user.address.street}
         </div>
