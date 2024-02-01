3fb5fa672bba271f97d5edb3c9da14736ef98610
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-01 03:05:23 +0300

    add drop functions

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -2,23 +2,42 @@ import React from 'react';
 
 const EventsExample: FC = () => {
     const [value, setValue] = React.useState<string>('')
-    const handleClick = (e: React.ChangeEvent<HTMLInputElement >) => {
+    const [isDrag, setIsDrag] = React.useState<boolean>(false)
+    const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
         setValue(e.target.value)
     }
-    const clickHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
+    const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
         console.log(value)
     }
-    const dragHangler = (e:React.DragEvent<HTMLDivElement>) => {
-        e.dataTransfer.
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log("DRAG")
+    }
+    const dragWithPreventHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(true)
+    }
+
+    const leaveHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(false)
+    }
+    const dropHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(false)
     }
     return (
         <div>
             <input type="text" value={value} onChange={handleClick}/>
             <button onClick={clickHandler}>вапвапвап</button>
-            <div style={{width: 200, height: 200, background: 'red'}} onDrag={dragHangler}></div>
-            <div style={{width: 200, height: 200, background: 'red', marginTop: 15}}></div>
+            <div draggable style={{width: 200, height: 200, background: 'red'}} onDrag={dragHandler}></div>
+            <div
+                onDragOver={dragWithPreventHandler}
+                onDrop={dropHandler}
+                onDragLeave={leaveHandler}
+                style={{width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15}}>
+            </div>
         </div>
     );
 };
